17d75a8f9f9e90c6d3cfbe97f9d3b86bead53189
Konrad <konrad.sochacki@wp.pl>
2024-01-06 14:33:30 +0100

    Szescian z obrotem

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 const char* vertexShaderSource = R"(
     #version 330 core
     layout (location = 0) in vec3 aPos;
     layout (location = 1) in vec3 aColor;
     out vec3 ourColor;
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
     void main()
     {
-        gl_Position = vec4(aPos, 1.0);
+        gl_Position = projection * view * model * vec4(aPos, 1.0);
         ourColor = aColor;
     }
 )";
@@ -29,20 +35,17 @@ void processInput(GLFWwindow* window);
 
 int main()
 {
-    // Inicjalizacja GLFW
     if (!glfwInit())
     {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
 
-    // Konfiguracja GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Tworzenie okna GLFW
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Kwadrat", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Szeœcian", nullptr, nullptr);
     if (!window)
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -52,17 +55,14 @@ int main()
 
     glfwMakeContextCurrent(window);
 
-    // Inicjalizacja GLEW
     if (glewInit() != GLEW_OK)
     {
         std::cerr << "Failed to initialize GLEW" << std::endl;
         return -1;
     }
 
-    // Ustawienie widoku OpenGL
     glViewport(0, 0, 800, 600);
 
-    // Kompilacja i linkowanie shaderów
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
     glCompileShader(vertexShader);
@@ -79,96 +79,113 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // Definicja wierzcho³ków kwadratu
     float vertices[] = {
-        // Wierzcho³ki        // Kolory
-        0.5f,  0.5f, 0.0f,    1.0f, 0.0f, 0.0f,  // Prawy górny
-        0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,  // Prawy dolny
-       -0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,  // Lewy dolny
-       -0.5f,  0.5f, 0.0f,    1.0f, 1.0f, 0.0f   // Lewy górny
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f
     };
 
-    // Indeksy wierzcho³ków dla kwadratu
     GLuint indices[] = {
-        0, 1, 3,  // Pierwszy trójk¹t
-        1, 2, 3   // Drugi trójk¹t
+        0, 1, 2, 2, 3, 0,
+        4, 5, 6, 6, 7, 4,
+        0, 3, 7, 7, 4, 0,
+        1, 2, 6, 6, 5, 1,
+        3, 2, 6, 6, 7, 3,
+        0, 1, 5, 5, 4, 0
     };
 
-    // Utworzenie i skonfigurowanie buforów OpenGL
     GLuint VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
 
-    // Wiazanie VAO
     glBindVertexArray(VAO);
 
-    // Wiazanie VBO i przekazanie danych wierzcho³kowych
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Wiazanie EBO i przekazanie danych indeksów
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // Konfiguracja atrybutów pozycji i kolorów wierzcho³ków
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    // Odwiazanie VAO
     glBindVertexArray(0);
 
-    // Ustawienie callbacka na zmianê rozmiaru okna
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Pêtla g³ówna
     while (!glfwWindowShouldClose(window))
     {
-        // Sprawdzanie wejœcia
         processInput(window);
 
-        // Czyszczenie bufora kolorów
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // U¿ycie shadera
+        glEnable(GL_DEPTH_TEST);
+
         glUseProgram(shaderProgram);
 
-        // Wiazanie VAO
+        // Wspó³rzêdne kamery
+        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+        glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+        glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+        // Macierz widoku
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+        // Macierz projekcji
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+        // Macierz modelu (obrót wokó³ osi Y)
+        static float angle = 0.0f;
+        angle += 0.5f;
+        glm::mat4 model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
+
+        // Macierz transformacji (model * view * projection)
+        glm::mat4 mvp = projection * view * model;
+
+        // Przekazanie macierzy transformacji do shadera
+        GLint modelLoc = glGetUniformLocation(shaderProgram, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(mvp));
+
+        GLint viewLoc = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        GLint projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
         glBindVertexArray(VAO);
 
-        // Rysowanie kwadratu
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
-        // Odwiazanie VAO
         glBindVertexArray(0);
 
-        // Zamiana buforów
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // Zwolnienie zasobów
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
 
-    // Zamkniêcie GLFW
     glfwTerminate();
 
     return 0;
 }
 
-// Funkcja obs³uguj¹ca zmianê rozmiaru okna
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
 
-// Funkcja obs³uguj¹ca wejœcie u¿ytkownika
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
